b3ced90cf11597dfc7ab70f0cc9635ded18adcd1
Akkarim <luis.carvajalrojas@ucr.ac.cr>
2019-05-18 18:44:02 -0600

    Working on methods

--- blueNodeTable.py
+++ blueNodeTable.py
@@ -5,20 +5,43 @@ import socket
 Graph = []
 # Diccionary for nodes (key=id content = ip:addr)
 nodeMap ={} 
+
 class blueNodeTable():
 
-
-    def genGraph():
+    #Generates the graph
+    def genGraph(self):
         with open('Grafo_Referencia.csv', newline='') as File:  
             reader = csv.reader(File)
             for row in reader:
                 #print(row) Test
                 Graph.append(row)
-
-    def genNodeMap()
+        return Graph
     
+    #Generates the nodemap
+    # x for default 
+    # -1 for requested
+    def genNodeMap(self, MAX_NODES):
+        for i in range(0, MAX_NODES):
+            nodeMap[i] = "x"
+        return nodeMap
+ 
+    # Write, after the accept and confirm the addr(string)
+    def write(self, node, addr):
+        nodeMap[node] = addr
 
+    #Return a list with the neig...
+    def retrieveNeighbors(self, node):
+        return Graph[node]
 
+    #Returns neighbors with their address
+    #def retrieveNeighbors(self, node):
+
+    #Returns node addr
+    def retrieveAddr(self, node):
+        if availableNode():
+            return nodeMap[node]
+        else:
+            return "The requested node isn't available"
 
 '''
     #Check if the node is avilable
@@ -30,4 +53,9 @@ class blueNodeTable():
 '''
 
 
-#if __name__== "__main__":
+
+if __name__== "__main__":
+
+    tables = blueNodeTable()
+    print(tables.genGraph())
+    print(tables.genNodeMap(5))
