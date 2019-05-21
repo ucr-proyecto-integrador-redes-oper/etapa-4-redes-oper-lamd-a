24e3f42b34354621ff4e99fd9710ebbffae780e4
dieg0cr98 <dieg0cr98@gmail.com>
2019-05-20 20:01:05 -0600

    main.py deleted a variable

--- NodoNaranja/main.py
+++ NodoNaranja/main.py
@@ -3,5 +3,5 @@ from orangeNode import orangeNode
 
 if __name__== "__main__":
 
-	orange = orangeNode('0.0.0.0',8888,2,6,"routingTable.txt", "Grafo_Referencia.csv")
+	orange = orangeNode('0.0.0.0',8888,2,"routingTable.txt", "../Grafo_Referencia.csv")
 	orange.run()
