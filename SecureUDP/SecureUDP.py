9f40d33c18d2389bc0b4c2d587ca45af8cbc4f07
dieg0cr98 <dieg0cr98@gmail.com>
2019-06-25 22:38:08 -0600

    fixed some bugs with the Udp and added the ooPack for testing

--- SecureUDP/SecureUDP.py
+++ SecureUDP/SecureUDP.py
@@ -3,6 +3,7 @@ import struct
 import socket as socket
 import queue
 import threading
+from ooPackage import ooPackage
 
 class PacketStruct:
 	def __init__(self, ip=0, port=0, payload=b'', sn=0, timeStamp=0.0):
@@ -32,9 +33,10 @@ class SecureUdp:
 		MOD: ---
 	'''
 
-	def __init__(self, window_size, ip, port):
+	def __init__(self, window_size,timeout, ip, port):
 		self.window_size = window_size
-
+		self.TIMEOUT = timeout
+		self.MAX_WINDOW_SIZE = window_size
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Socket UDP
 		self.sock.bind((ip, port))
 		##Creates the Threads
@@ -56,7 +58,7 @@ class SecureUdp:
 	def checkTimeStamps(self):  # resuelve todos los problemas, hasta nachos gg izi pici
 		to_delete = []
 		for item in self.AckWindow:
-			if (self.AcksReceived.count(item.sn + 1) > 0):
+			if (self.AcksReceived.count(item.sn) > 0):
 				#print("Put shit in the ackreceived queue")
 				to_delete.append(item)  # Haven't received the ack
 			elif ((time.time() - item.timeStamp) > self.TIMEOUT):
@@ -65,10 +67,9 @@ class SecureUdp:
 				# resets the timestamp with the current time since we just resend it.
 				item.timeStamp = time.time()
 
-
+		
 		for deleted in to_delete:  # Solo borra lo que hay en todelete
 			self.AckWindow.remove(deleted)
-			print("Removed an ACK")
 
 	'''
 		EFE: Devuelve el SNRN evitando que el mismo sea mas grande que la capacidad de un short
@@ -93,10 +94,11 @@ class SecureUdp:
 					ip,port,payload = self.waiting_queue.pop(0)
 					client = (ip, port)
 					#self.SNRN = self.nextSNRN(self.SNRN)
-					modified_payload = struct.pack('!bH', 0, self.SNRN) + payload
-					print("Sending ",payload," To ",ip,port,modified_payload)
+					modified_payload = struct.pack('!bH',0, self.SNRN) + payload
+					#print("Sending ",payload," To ",ip,port,modified_payload)
 					self.sock.sendto(modified_payload, client)   # Envía!!!
 					self.AckWindow.append(PacketStruct(ip, port, modified_payload, self.SNRN, time.time()))
+					#self.SNRN = self.SNRN + 1
 					self.SNRN = self.nextSNRN(self.SNRN)
 
 			self.checkTimeStamps()
@@ -119,49 +121,58 @@ class SecureUdp:
 
 
 	def recivefrom(self):
-		return self.reciveQueue.get(block=True,timeout=None)
+		payload =  self.reciveQueue.get(block=True,timeout=None)
+		return payload[3:]
 		
 
 	def dummtReceive(self):
 		while True:  # matiene la conexión
 			payload, client_addr = self.sock.recvfrom(5000)  # Buffer size
-			#print("i just receive ",payload," from client ",client_addr)
+			print("i just receive ",payload," from client ",client_addr)
 			if int.from_bytes(payload[:1], byteorder='big') == 0:
 				# THIS ISNT GONNA WORK ON FIRST TRY
-				sn_received = int.from_bytes(payload[0:2],byteorder='big')
-				print("\T Received: %s with SN %d" % (payload[0:2], sn_received))
-				sn_to_send = self.nextSNRN(sn_received)
+				sn_received = int.from_bytes(payload[1:3],byteorder='big')
+				#sn_to_send = self.nextSNRN(sn_received)
 				#print("\tsending ACK with SN: %d" % (sn_to_send)) 
-				ack_payload = struct.pack('!bH', 1, sn_to_send)
+				ack_payload = struct.pack('!bH', 1, sn_received)
 				#print(payload)
 				self.sock.sendto(ack_payload, client_addr)
 				self.reciveQueue.put(payload)
 
 			else:
-				ACK_received = int.from_bytes(payload[1:3],byteorder='big') + 1
-				print("Received: ACK ", ACK_received)
+				ACK_received = int.from_bytes(payload[1:3],byteorder='big')
+				print("Received: ACK ", ACK_received,payload)
 				#print("ACK Payload: ", payload)
-				self.AcksReceived.append(int.from_bytes(payload[1:3],byteorder='big'))
+				self.AcksReceived.append(ACK_received)
 
 
 
 
 def main():
-	ip = input("My ip ")
+#	ip = input("My ip ")
 	port = input("My port ")
-	ip2 = input("other ip ")
+#	ip2 = input("other ip ")
 	port2 = input("other port ")
-	test = SecureUdp(4,ip,int(port))
-	payload = b'luIS'
+
+
+	ooPackagex = ooPackage(2,1,3,9,'r',566,'0.0.0.0',8888,1000)
+	serializedObject = ooPackagex.serialize()
+
+
+	test = SecureUdp(100,4,"0.0.0.0",int(port))
+	#payload = b'\x13'
 
 	
 	for item in range(2):
-		test.dummySendto(payload,ip2,int(port2))
+		test.dummySendto(serializedObject,"0.0.0.0",int(port2))
 	
 	while True:
 		payload = test.recivefrom()
-		print("Receive ",payload)
+		print("Capa superior ",payload)
+		ooPackage2 = ooPackage()
+		ooPackage2.unserialize(payload)
+		ooPackage2.print_data()
 
 
 if __name__ == "__main__":
-    main()
+	main()
