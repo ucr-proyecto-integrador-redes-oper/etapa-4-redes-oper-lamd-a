985c350960bf279d9cd108447c50fa715c35f4f2
dieg0cr98 <dieg0cr98@gmail.com>
2019-06-29 20:08:47 -0600

    added logicThread to the blueNode

--- NodoNaranja/blueNode.py
+++ NodoNaranja/blueNode.py
@@ -9,67 +9,75 @@ class blueNode:
     sTreeDadNode = -1
     sTreeSonsNodes = []
     myID = 0
-    packageQueue = queue.Queue() # Tuple with
+    packageQueue = queue.Queue() # Tuple with (bytePackage,addr). Addr is a tuple with the ip and port 
+    blueSavedChunks = [] #Tuple with (ArchivoID,ChunkID,Chunk). Cant be more than 40
+    SecureUdp
 
     def __init__(self,MyIP,MyPort,OtherIP,OtherPort):
-        SecureUdpOrange = SecureUdp(10,4,MyIP,MyPort) #ventana de 10 con timeout de 2s
-        # Creates the Thread
-        t = threading.Thread(target=self.inputThread, args=(SecureUdpOrange,))
+        SecureUDP = SecureUdp(10,4,MyIP,MyPort) #ventana de 10 con timeout de 2s
+        # Creates the Threads
+        t = threading.Thread(target=self.inputThread, args=(SecureUDP,))
         t.start()
+
+        t2 = threading.Thread(target=self.logicalThread, args=(SecureUDP,))
+        t2.start()
+
 
         obPackagex = obPackage(14)
         serializedObject = obPackagex.serialize(14)
-        SecureUdpOrange.sendto(serializedObject,OtherIP,OtherPort)
+        SecureUDP.sendto(serializedObject,OtherIP,OtherPort)
 
-    def inputThread(self,SecureUdpOrange):
+    def inputThread(self,SecureUDP):
         while True:
-            payload , addr = SecureUdpOrange.recivefrom()
+            payload , addr = SecureUDP.recivefrom()
+            print(payload)
             self.packageQueue.put((payload,addr))
 
 
 
 
-    def logicalThread(self):
+    def logicalThread(self,SecureUDP):
         while True:
             package = self.packageQueue.get(block=True,timeout=None)
-            bytePackage = package(0)
+            print(package)
+            bytePackage = package[0]
             Type = int.from_bytes(bytePackage[:1], byteorder='big')
             if Type == 0:
-                print("Tipo put chunk")
+                print("(PutChunk) from ",package[1])
+                chunkPack = obPackage()
+                chunkPack.unserialize(bytePackage,0)
+                #If theres less than 40 chunks saved, then save the chunk
+                if len(blueSavedChunks) < 40:
+                    blueSavedChunks.append(chunkPack.chunk)
+                #Otherwise it sends the chunk to the neighbors
+                else:
+                    #Creates a putChunk package
+                    serializedPutChunkPack = chunkPack.serialize(0)
+                    for neighbor in neighborTuple:
+                        SecureUDP.sendto(serializedPutChunkPack,neighbor[1],neighbor[2])
+
+
             elif Type == 1:
-                print("Tipo hello")
-            elif Type == 1:
-                print(Tipo hello)
-            elif Type == 1:
-                print(Tipo hello)
-            elif Type == 1:
-                print(Tipo hello)
-            elif Type == 1:
-                print(Tipo hello)
-            elif Type == 1:
-                print(Tipo hello)
-            elif Type == 1:
-                print(Tipo hello)
+                print("(Hello) from ",package[1])
             elif Type == 15:
-                print("Tipo neighbor without address")
-                obPackagex.unserialize(payload,15)
+                print("(NeighborNoAddrs) from ",package[1])
+                obPackagex = obPackage()
+                obPackagex.unserialize(bytePackage,15)
                 self.neighborTuple.append((obPackagex.neighborID,"0.0.0.0",-1))
                 self.myID = obPackagex.nodeID
             elif Type == 16:
-                print("Tipo neighbor with address")
+                print("(Neighbor) from ",package[1])
                 obPackagex = obPackage()
-                obPackagex.unserialize(payload,16)
+                obPackagex.unserialize(bytePackage,16)
                 self.neighborTuple.append((obPackagex.neighborID,obPackagex.blueAddressIP,obPackagex.blueAddressPort))
                 self.myID = obPackagex.nodeID
                 helloPack = obPackage(1)
                 serializedHelloPack = helloPack.serialize(1)
-                SecureUdpOrange.sendto(serializedHelloPack,obPackagex.blueAddressIP,obPackagex.blueAddressPort)
+                SecureUDP.sendto(serializedHelloPack,obPackagex.blueAddressIP,obPackagex.blueAddressPort)
 
             elif Type == 17:
-                print("Tipo GraphComplete")
-                # Creates the Thread
-                t = threading.Thread(target=self.logicalThread, args=())
-                t.start()
+                print("(GraphComplete) from ",package[1])
+
 
 
 
